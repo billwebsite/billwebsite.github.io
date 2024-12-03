38ee0f539d8dcba414d7bab693c47d84c335c5d3
Truong Ngoc Chau <billtruong003@gmail.com>
2024-12-04 00:27:51 +0700

    Optimize

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-// Đợi cho DOM được tải xong
 window.addEventListener('DOMContentLoaded', () => {
-
     // Kích hoạt Bootstrap scrollspy trên phần tử sidebar
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
@@ -8,22 +6,21 @@ window.addEventListener('DOMContentLoaded', () => {
             target: '#sideNav',
             rootMargin: '0px 0px -40%',
         });
-    };
+    }
 
     // Thu nhỏ navbar khi người dùng nhấp vào các liên kết trên mobile
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = document.querySelectorAll('#navbarResponsive .nav-link');
-    
-    // Lưu kiểu hiển thị của navbarToggler để tránh gọi `getComputedStyle` trong vòng lặp
-    const isNavbarVisible = window.getComputedStyle(navbarToggler).display !== 'none';
 
-    responsiveNavItems.forEach(responsiveNavItem => {
-        responsiveNavItem.addEventListener('click', () => {
-            // Chỉ click navbarToggler khi nó hiển thị
-            if (isNavbarVisible) {
+    // Lưu trạng thái của navbarToggler
+    const isNavbarVisible = navbarToggler && window.getComputedStyle(navbarToggler).display !== 'none';
+
+    if (isNavbarVisible) {
+        // Đảm bảo click navbarToggler chỉ khi nó hiển thị
+        responsiveNavItems.forEach(responsiveNavItem => {
+            responsiveNavItem.addEventListener('click', () => {
                 navbarToggler.click();
-            }
+            });
         });
-    });
-
+    }
 });
